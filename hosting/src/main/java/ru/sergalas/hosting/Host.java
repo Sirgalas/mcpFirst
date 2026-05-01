b137413d513dc6e6826540aef6e39de6e4541d13
Sergalas <svbeloventcev@sberinsur.ru>
2026-05-01 11:02:02 +0300

    4.2

--- hosting/src/main/java/ru/sergalas/hosting/Host.java
+++ hosting/src/main/java/ru/sergalas/hosting/Host.java
@@ -1,9 +1,16 @@
 package ru.sergalas.hosting;
 
+import io.modelcontextprotocol.client.McpClient;
+import io.modelcontextprotocol.client.McpSyncClient;
+import io.modelcontextprotocol.client.transport.HttpClientStreamableHttpTransport;
+import io.modelcontextprotocol.spec.McpSchema;
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.sergalas.hosting.util.CallToolUtil;
 
 @Service
 public class Host {
@@ -12,11 +19,22 @@ public class Host {
     ChatClient chatClient;
 
 
+    private String systemPrompt;
+    private McpSyncClient client;
+
+    @PostConstruct
+    public void init() {
+        HttpClientStreamableHttpTransport transport = HttpClientStreamableHttpTransport.builder("http://localhost:8091").endpoint("mcp").build();
+        client = McpClient.sync(transport).build();
+        client.initialize();
+        McpSchema.ListToolsResult toolsResult = client.listTools();
+        systemPrompt = SystemPromptFactory.withTools(toolsResult);
+    }
+
+
     public void printAnswerToUser(String question){
-       chatClient.prompt().system()
-
-
-
+        AssistantMessage assistantMessage = chatClient.prompt().system(systemPrompt).user(question).call().chatResponse().getResult().getOutput();
+        if(CallToolUtil.isToolRequired(assistantMessage.getText())){}
 
     }
 }
--- hosting/src/main/java/ru/sergalas/hosting/util/CallToolUtil.java
+++ hosting/src/main/java/ru/sergalas/hosting/util/CallToolUtil.java
@@ -1,6 +1,7 @@
 package ru.sergalas.hosting.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jspecify.annotations.Nullable;
 
 import java.util.regex.Pattern;
 
@@ -10,4 +11,8 @@ public class CallToolUtil {
 
     private static final Pattern TOOL_CALL_PATTERN = Pattern.compile("<tool_call>\\s*(\\{.*?})\\s*</tool_call>",
             Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+
+    public static boolean isToolRequired(String text) {
+        return TOOL_CALL_PATTERN.matcher(text).find();
+    }
 }
